2cf351b7298e126839346d3e622860264af9f920
Beatriz Rivero López <brivlop@upv.edu.es>
2026-05-22 11:32:05 +0200

    Existen 5 categorías por defecto

---
+++ VotifyIU.Client/Services/CategoriasPorDefecto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+namespace VotifyIU.Client.Services
+{
+    public class CategoriasPorDefecto
+    {
+        public static IReadOnlyCollection<String> Todas { get; } = new[] {
+            "Medio Ambiente"
+            , "Educación"
+            , "Salud"
+            , "IA"
+            , "Fintech"
+        };
+        }
+    }
+
